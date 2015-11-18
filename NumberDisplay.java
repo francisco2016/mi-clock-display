3cc3171270a4889b2cb49ff59c2a080267a9614f
francisco2016 <otrayama@gmail.com>
2015-11-18 12:28:05 +0100

    creada clase NumberDisplay con atributos y constructor.

---
+++ NumberDisplay.java
@@ -0,0 +1,52 @@
+
+/**
+ * Write a description of class NumberDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NumberDisplay
+{
+    // At. para crear un valor.
+    private int valor;
+    // At. para crear un límite.
+    private int limite;
+
+    /**
+     * Constructor for dar valor  a los atributos.
+     */
+    public NumberDisplay(int valorLimite)
+    {
+        valor = 0;
+        limite = valorLimite;
+    }
+    
+//     /**
+//      * Mt.para fijar el valor actual del display.
+//      */
+//     public void setValue(int valorV){
+//         if( (valorV >= 0) && (valorV < limite)){
+//             valor = valorV;
+//         }
+//    
+//     
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
